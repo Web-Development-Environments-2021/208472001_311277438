35cc401986775a13e617b7048b490507696d193b
amit yosefi <amit.yosefi@gmail.com>
2021-04-21 17:38:10 +0300

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var context;
-var shape = new Object();
-var board;
-var score;
-var pac_color;
-var start_time;
-var time_elapsed;
-var interval;
+let context;
+let shape = new Object();
+let board;
+let score;
+let pac_color;
+let start_time;
+let time_elapsed;
+let interval;
+let current;
+let last_press_direction;
 
 $(document).ready(function() {
 	context = canvas.getContext("2d");
@@ -16,14 +18,14 @@ function Start() {
 	board = new Array();
 	score = 0;
 	pac_color = "yellow";
-	var cnt = 100;
-	var food_remain = 50;
-	var pacman_remain = 1;
+	let cnt = 100;
+	let food_remain = 5;
+	let pacman_remain = 1;
 	start_time = new Date();
-	for (var i = 0; i < 10; i++) {
+	for (let i = 0; i < 10; i++) {
 		board[i] = new Array();
 		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
-		for (var j = 0; j < 10; j++) {
+		for (let j = 0; j < 10; j++) {
 			if (
 				(i == 3 && j == 3) ||
 				(i == 3 && j == 4) ||
@@ -33,7 +35,7 @@ function Start() {
 			) {
 				board[i][j] = 4;
 			} else {
-				var randomNum = Math.random();
+				let randomNum = Math.random();
 				if (randomNum <= (1.0 * food_remain) / cnt) {
 					food_remain--;
 					board[i][j] = 1;
@@ -49,8 +51,9 @@ function Start() {
 			}
 		}
 	}
+	Draw(4)
 	while (food_remain > 0) {
-		var emptyCell = findRandomEmptyCell(board);
+		let emptyCell = findRandomEmptyCell(board);
 		board[emptyCell[0]][emptyCell[1]] = 1;
 		food_remain--;
 	}
@@ -69,12 +72,12 @@ function Start() {
 		},
 		false
 	);
-	interval = setInterval(UpdatePosition, 250);
+	interval = setInterval(UpdatePosition, 150);
 }
 
 function findRandomEmptyCell(board) {
-	var i = Math.floor(Math.random() * 9 + 1);
-	var j = Math.floor(Math.random() * 9 + 1);
+	let i = Math.floor(Math.random() * 9 + 1);
+	let j = Math.floor(Math.random() * 9 + 1);
 	while (board[i][j] != 0) {
 		i = Math.floor(Math.random() * 9 + 1);
 		j = Math.floor(Math.random() * 9 + 1);
@@ -97,25 +100,76 @@ function GetKeyPressed() {
 	}
 }
 
-function Draw() {
+function Drawplayer(center, x){
+	if (x == 1){
+		context.arc(center.x, center.y, 30, 1.65 * Math.PI, 1.35 * Math.PI); // up
+		context.lineTo(center.x, center.y);
+		context.fillStyle = pac_color; //color
+		context.fill();
+		
+		context.beginPath();
+		context.arc(center.x + 17, center.y , 5, 0, 2 * Math.PI); // circle
+		context.fillStyle = "black"; //color
+		context.fill();
+		last_press_direction = 1;
+	}
+	if (x == 2){
+		context.arc(center.x, center.y, 30, 0.65 * Math.PI, 0.35 * Math.PI); // down
+		context.lineTo(center.x, center.y);
+		context.fillStyle = pac_color; //color
+		context.fill();
+		
+		context.beginPath();
+		context.arc(center.x + 15, center.y - 5, 5, 0, 2 * Math.PI); // circle
+		context.fillStyle = "black"; //color
+		context.fill();
+		last_press_direction = 2;
+	}
+	if (x == 3){
+		context.arc(center.x, center.y, 30, 1.20 * Math.PI, 0.85 * Math.PI); // left
+		context.lineTo(center.x, center.y);
+		context.fillStyle = pac_color; //color
+		context.fill();
+		
+		context.beginPath();
+		context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
+		context.fillStyle = "black"; //color
+		context.fill();
+		last_press_direction = 3;
+	}
+	if (x == 4){
+		context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // right
+		context.lineTo(center.x, center.y);
+		context.fillStyle = pac_color; //color
+		context.fill();
+		
+		context.beginPath();
+		context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
+		context.fillStyle = "black"; //color
+		context.fill();
+		last_press_direction = 4;
+	}
+}
+
+
+function Draw(x) {
+	let moves = [1, 2, 3, 4];
 	canvas.width = canvas.width; //clean board
 	lblScore.value = score;
 	lblTime.value = time_elapsed;
-	for (var i = 0; i < 10; i++) {
-		for (var j = 0; j < 10; j++) {
-			var center = new Object();
+	for (let i = 0; i < 10; i++) {
+		for (let j = 0; j < 10; j++) {
+			let center = new Object();
 			center.x = i * 60 + 30;
 			center.y = j * 60 + 30;
 			if (board[i][j] == 2) {
 				context.beginPath();
-				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
-				context.lineTo(center.x, center.y);
-				context.fillStyle = pac_color; //color
-				context.fill();
-				context.beginPath();
-				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
-				context.fill();
+				if (moves.includes(x,0))
+					Drawplayer(center, x);
+				else
+					Drawplayer(center, last_press_direction);
+				
+
 			} else if (board[i][j] == 1) {
 				context.beginPath();
 				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
@@ -132,25 +186,26 @@ function Draw() {
 }
 
 function UpdatePosition() {
+
 	board[shape.i][shape.j] = 0;
-	var x = GetKeyPressed();
+	let x = GetKeyPressed();
 	if (x == 1) {
-		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
+		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) { //up
 			shape.j--;
 		}
 	}
 	if (x == 2) {
-		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
+		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) { //down
 			shape.j++;
 		}
 	}
 	if (x == 3) {
-		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
+		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) { //left
 			shape.i--;
 		}
 	}
 	if (x == 4) {
-		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
+		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) { //right
 			shape.i++;
 		}
 	}
@@ -158,15 +213,15 @@ function UpdatePosition() {
 		score++;
 	}
 	board[shape.i][shape.j] = 2;
-	var currentTime = new Date();
+	let currentTime = new Date();
 	time_elapsed = (currentTime - start_time) / 1000;
 	if (score >= 20 && time_elapsed <= 10) {
 		pac_color = "green";
 	}
-	if (score == 50) {
+	if (score >= 100) {
 		window.clearInterval(interval);
-		window.alert("Game completed");
+		window.alert("Winner!!!");
 	} else {
-		Draw();
+		Draw(x);
 	}
 }
